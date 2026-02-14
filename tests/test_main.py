68ab1af478ffd1fe6841f73c6b77ba5011885b7a
Brock Maddock <brock@scean.lan>
2026-02-13 20:36:32 -0700

    added new function to check for happiness at WeatherPy :)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 # import pytest
-from src.main import conversion, is_strong
+from src.main import conversion, is_strong, is_happy
 
 def test_conversion():
     # 100C should be 212F
@@ -12,3 +12,8 @@ def test_logic():
     # Carl's code returns True because 60 > 50.
     data = [40, 40, 60]
     assert is_strong(data) is False
+
+def test_happy():
+    # We are always happy at WeatherPy!
+    assert is_happy([0, 0, 0]) is True
+    assert is_happy([100, 100, 100]) is True
